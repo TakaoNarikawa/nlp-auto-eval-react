b6ac7491cf37ddb732b2e48f4f1e36d40afca7c7
s2terminal <suzuki.sh@s2terminal.com>
2022-04-23 23:10:17 +0900

    fix some expressions

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ import { useState } from 'react';
 import { bleu, tokenizer, nGramPrecisions, brevityPenalty } from '../lib/text';
 
 const Home: NextPage = () => {
-  const [textHypothesis, setHypValue] = useState("this is a pen");
-  const [textReferences, setRefValue] = useState("this is a apple");
+  const [textHypothesis, setHypValue] = useState("this is a pen .");
+  const [textReferences, setRefValue] = useState("this is a apple .");
   const bleuN = 4;
 
   const tokensHyp = tokenizer(textHypothesis);
@@ -27,10 +27,11 @@ const Home: NextPage = () => {
       <Head>
         <title>BLEU計算するやつ</title>
         <meta name="description" content="BLEU計算するやつ" />
-        <link rel="icon" href="/icon.png" />
+        <link rel="icon" href="icon.png" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css" />
       </Head>
       <h1>BLEU計算するやつ</h1>
+      <p>半角スペースで区切られた文章を入力するとBLEUを計算します。</p>
       <div>
         生成文:
         <textarea rows={2} value={textHypothesis} onChange={handleChangeGen} />
@@ -38,31 +39,31 @@ const Home: NextPage = () => {
       <div>
         参照文:
         <textarea rows={2} value={textReferences} onChange={handleChangeRef} />
-      </div>
-      <p>
         (N={bleuN})
-        <table>
-          <tbody>
-            {Object.keys(precisions).map(i => {
-              return <tr key={i}>
-                <th>P{i}</th>
-                <td>{precisions[Number(i)].match}/{precisions[Number(i)].total}</td>
-              </tr>;
-            })}
-            <tr><th>BP</th><td>{bp}</td></tr>
-            <tr><th>BLEU</th><td>{bleu(tokenizer(textHypothesis), tokenizer(textReferences), 4)}</td></tr>
-          </tbody>
-        </table>
-      </p>
+      </div>
+      <table>
+        <tbody>
+          {Object.keys(precisions).map(i => {
+            return <tr key={i}>
+              <th>Precision {i}</th>
+              <td>{precisions[Number(i)].match}/{precisions[Number(i)].total}</td>
+            </tr>;
+          })}
+          <tr><th>Brevity Penalty</th><td>{bp}</td></tr>
+          <tr><th>BLEU</th><td>{bleu(tokenizer(textHypothesis), tokenizer(textReferences), 4)}</td></tr>
+        </tbody>
+      </table>
       <div>
         <h3>参考文献</h3>
         <ul>
           <li>
-            <a href="https://cloud.google.com/translate/automl/docs/evaluate?hl=ja#bleu" target="_blank" rel="noreferrer">
+            <a href="https://cloud.google.com/translate/automl/docs/evaluate?hl=ja#bleu" target="_blank" rel="noreferrer" >
               モデルの評価  |  AutoML Translation のドキュメント  |  Google Cloud
             </a>
           </li>
+          <li><a href="https://en.wikipedia.org/wiki/BLEU" target="_blank">BLEU - Wikipedia</a></li>
         </ul>
+        リポジトリ: <a href="https://github.com/s2terminal/nlp-auto-eval-react" target="_blank">s2terminal/nlp-auto-eval-react</a>
       </div>
     </div >
   );
