73335602238d7ece0beabcaa81f26f346ae0b517
s2terminal <suzuki.sh@s2terminal.com>
2022-04-23 22:19:34 +0900

    fix static build path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.GITHUB_PAGES ? '/nlp-auto-eval-react' : '',
 }
 
 module.exports = nextConfig
