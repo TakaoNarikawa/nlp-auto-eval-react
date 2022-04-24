f3ba14b467b1b4a6e10fc5bc133dea901be8b280
s2terminal <suzuki.sh@s2terminal.com>
2022-04-24 16:24:30 +0900

    set default value

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import { bleu, tokenizer, nGramPrecisions, brevityPenalty } from '../lib/text';
 const Home: NextPage = () => {
   const [textHypothesis, setHypValue] = useState("this is a pen .");
   const [textReferences, setRefValue] = useState("this is a apple .");
-  const bleuN = 4;
+  const [bleuN, setBleuN] = useState(4);
 
   const tokensHyp = tokenizer(textHypothesis);
   const tokensRef = tokenizer(textReferences);
@@ -21,6 +21,27 @@ const Home: NextPage = () => {
   const handleChangeRef = (event: any) => {
     setRefValue(event.target.value);
   };
+  const handleChangeN = (event: any) => {
+    setBleuN(Math.max(1, Number(event.target.value)));
+  }
+  const handleChangeDefaultButton = (n: number) => {
+    return (_e: any) => {
+      switch (n) {
+        case 1:
+          setHypValue("This is a pen .");
+          setRefValue("This is an apple .");
+          break;
+        case 2:
+          setHypValue("A NASA rover is fighting a massive storm on Mars .");
+          setRefValue("The NASA Opportunity rover is battling a massive dust storm on Mars .");
+          break;
+        case 3:
+          setHypValue("吾輩 は 猫 で ある 。 名前 は まだ 無い 。");
+          setRefValue("私 は 猫 です 。 名前 は まだ あり ませ ん 。");
+          break;
+      }
+    }
+  }
 
   return (
     <div className="container">
@@ -33,12 +54,20 @@ const Home: NextPage = () => {
       <h1>BLEU計算するやつ</h1>
       <p>半角スペースで区切られた文章を入力するとBLEUを計算します。</p>
       <div>
-        生成文:
-        <textarea rows={2} value={textHypothesis} onChange={handleChangeGen} />
-      </div>
-      <div>
-        参照文:
-        <textarea rows={2} value={textReferences} onChange={handleChangeRef} />
+        n-gram: <input type="number" value={bleuN} onChange={handleChangeN} />
+        <div>
+          生成文:
+          <textarea rows={2} value={textHypothesis} onChange={handleChangeGen} />
+        </div>
+        <div>
+          参照文:
+          <textarea rows={2} value={textReferences} onChange={handleChangeRef} />
+        </div>
+        <p>
+          <a onClick={handleChangeDefaultButton(1)} >例1</a>,
+          <a onClick={handleChangeDefaultButton(2)} >例2</a>,
+          <a onClick={handleChangeDefaultButton(3)} >例3</a>
+        </p>
         (N={bleuN})
       </div>
       <table>
@@ -46,13 +75,17 @@ const Home: NextPage = () => {
           {Object.keys(precisions).map(i => {
             return <tr key={i}>
               <th>Precision {i}</th>
-              <td>{precisions[Number(i)].match}/{precisions[Number(i)].total}</td>
-            </tr>;
+              <td>
+                {precisions[Number(i)].match}/{precisions[Number(i)].total}
+                {precisions[Number(i)].match === 0 && "※"}
+              </td>
+            </tr>
           })}
           <tr><th>Brevity Penalty</th><td>{bp}</td></tr>
           <tr><th>BLEU</th><td>{bleu(tokenizer(textHypothesis), tokenizer(textReferences), 4)}</td></tr>
         </tbody>
       </table>
+      <p>※n-gramの一致数がゼロになっている場合、BLEUは適切でない数値になります。</p>
       <div>
         <h3>参考文献</h3>
         <ul>
