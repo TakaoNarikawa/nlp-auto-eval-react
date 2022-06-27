d14947cb48d1bf185939f61e1f1469f9ff25a15c
TakaoNarikawa <TakaoNarikawa@users.noreply.github.com>
2022-06-27 14:54:49 +0000

    deploy: 733906e40fb3c295e169f89f044d0f532bad9e71

---
+++ _next/static/H4jrms_XDf6F1oRbJ38Ix/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1f0d274cb06c8119.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
